51898fbdb97b79623b8d3444c3ee32ae53d5a79b
Ravi <rabigiri18@gmail.com>
2023-06-21 09:48:43 +0545

    feat: add common drawer

--- src/components/Main/Statements/StatementDrawer.tsx
+++ src/components/Main/Statements/StatementDrawer.tsx
@@ -1,13 +1,34 @@
 "use client";
 
-import { Button } from "antd";
-import { useState } from "react";
+import { Button, Drawer } from "antd";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { use, useState } from "react";
 
 export default function StatementDrawer() {
-  const [showDrawer, setShowDrawer] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const layoutSegment = useSelectedLayoutSegment();
   return (
-    <Button type="primary" size="large" onClick={() => setShowDrawer(true)}>
-      Add a Statement
-    </Button>
+    <>
+      <Button type="primary" size="large" onClick={() => setIsDrawerOpen(true)}>
+        Add a Statement
+      </Button>
+      <Drawer
+        width={720}
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+        footer={
+          <Button type="primary" size="large">
+            Upload
+          </Button>
+        }
+        title={
+          layoutSegment
+            ? `Upload ${
+                layoutSegment[0].toUpperCase() + layoutSegment.slice(1)
+              } Statement`
+            : "Upload Statement"
+        }
+      />
+    </>
   );
 }
