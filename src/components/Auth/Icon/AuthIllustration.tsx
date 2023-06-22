d5431b083182dfd6f03305651d730d196f96b858
Ravi <rabigiri18@gmail.com>
2023-06-22 09:28:08 +0545

    fix: import

--- src/components/Auth/Icon/AuthIllustration.tsx
+++ src/components/Auth/Icon/AuthIllustration.tsx
@@ -1,6 +1,6 @@
 "use client";
-import AuthIllustrationSvg from "@/icons/Auth/AuthIllustration.svg";
+import AuthIllustrationSvg from "@/icons/AuthIllustration.svg";
 
 export default function AuthIllustration() {
-  return <AuthIllustrationSvg className="" />;
+  return <AuthIllustrationSvg />;
 }
