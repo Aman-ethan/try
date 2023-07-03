2985266d7bea5cc2afa1a8fcc3ad0dacabe2da7d
Ravi <rabigiri18@gmail.com>
2023-07-03 09:45:35 +0545

    fix(#30): type error

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -6,22 +6,17 @@ import { TRANSACTION_SERVER_URL } from "@/constants/strings";
 
 function useQuery<Data>(
   key: string | null,
+  baseURL: string,
   config?: SWRConfiguration<Data, Error>
 ) {
-  return useSWR<Data, Error>(
-    key ? [key, "6e51548cebfc07b7531c540ebe5fbe18fbf00beb"] : null,
-    getFetcher,
-    {
-      ...config,
-    }
-  );
+  return useSWR<Data, Error>(key, getFetcher(baseURL), config);
 }
 
 export function useTransactionServerQuery<Data>(
   key: string | null,
   config?: SWRConfiguration<Data, Error>
 ) {
-  return useQuery<Data>(key ? TRANSACTION_SERVER_URL + key : null, config);
+  return useQuery<Data>(key, TRANSACTION_SERVER_URL, config);
 }
 
 export function useAnalyticsServerQuery() {}
