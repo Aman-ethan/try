a53da6a2603f975a9a45bac23c448334cf61c331
KrishEthan <138201250+KrishEthan@users.noreply.github.com>
2023-07-24 22:25:02 +0530

    Feat/change password v2 (#74)
    * feat: implemented change password screen and enabled routing.
    
    * feat: connected change-password functionality with backend.
    
    * fix: validation issue and restructured the code as commented.
    
    * chore: cleanup ChangePasswordForm.tsx
    
    * fix: lint check
    
    * fix: lint issue
    
    * fix: lint.
    
    ---------
    
    Co-authored-by: Krish-Parekh <krishparekh303@gmail.com>
    Co-authored-by: Ravi Giri <Regaron@users.noreply.github.com>
    Co-authored-by: Ravi <rabigiri18@gmail.com>

--- src/app/(dashboard)/change-password/layout.tsx
+++ src/app/(dashboard)/change-password/layout.tsx
@@ -1,7 +1,7 @@
 import { ILayoutProps } from "@/interfaces/Main";
 import { Title } from "@/lib/antd";
 
-export default function TradeLayout({ children }: ILayoutProps) {
+export default function ChangePasswordLayout({ children }: ILayoutProps) {
   return (
     <div className="h-[calc(100vh-4.625rem)] px-12 py-8">
       <div className="h-full space-y-10 rounded-lg bg-neutral-1 p-8">
