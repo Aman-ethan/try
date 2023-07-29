203e0b3fc3778ea5fb107cee66298fd6c57f10bf
Ravi <rabigiri18@gmail.com>
2023-07-29 16:22:06 +0545

    fix: icon

--- src/components/Icon/Website.tsx
+++ src/components/Icon/Website.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import LogoSvg from "@/icons/Logo.svg";
+import WebsiteSvg from "@/icons/Website.svg";
 import { IIconProps } from "@/interfaces/General";
 
 export default function Logo({ className }: IIconProps) {
-  return <LogoSvg className={className} />;
+  return <WebsiteSvg className={className} />;
 }
--- src/components/Main/ClientInformation/ClientEmpty.tsx
+++ src/components/Main/ClientInformation/ClientEmpty.tsx
@@ -1,12 +1,12 @@
 import Paragraph from "@/components/Typography/Paragraph";
 import Title from "@/components/Typography/Title";
-import WebsiteIllustration from "@/icons/Website.svg";
+import Website from "@/components/Icon/Website";
 import AddClient from "./AddClient";
 
 export default function ClientEmpty() {
   return (
     <div className="bg-white flex flex-col items-center min-h-[calc(100vh-14rem)] py-12 tab:py-20 px-4 tab:px-10 gap-y-10 rounded-lg">
-      <WebsiteIllustration />
+      <Website />
       <div className="max-w-[40rem] space-y-8 text-center">
         <div className="space-y-4">
           <Title level={4}>Welcome to EthanAI!</Title>
