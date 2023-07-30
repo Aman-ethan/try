c9928c8f84b4941d6083b1edaf6760e1fc3ec728
Ravi <rabigiri18@gmail.com>
2023-07-30 18:40:16 +0545

    feat: add font

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,13 +1,22 @@
 import { ReactNode } from "react";
+import { Roboto } from "next/font/google";
 import DashboardLayout from "@/components/Main/Layout/DashboardLayout";
 import AppProvider from "@/context/AppProvider";
 
 import "antd/dist/reset.css";
 import "@/styles/global.css";
 
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
 export default function MainRootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body id="layout" className="opacity-0 transition-opacity">
         <AppProvider>
           <DashboardLayout>{children}</DashboardLayout>
