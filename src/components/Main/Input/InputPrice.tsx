03c4baac72d6f3bec104257d5d223cf77a0073aa
Ravi <rabigiri18@gmail.com>
2023-08-03 17:02:20 +0545

    chore: revert input_price

--- src/components/Main/Input/InputPrice.tsx
+++ src/components/Main/Input/InputPrice.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { InputNumberProps } from "antd";
-import { formatPrice } from "@/lib/format";
+import { formatPriceWithSymbol } from "@/lib/format";
 import InputNumber from "./InputNumber";
 
 export default function InputPrice({
@@ -11,7 +11,9 @@ export default function InputPrice({
   return (
     <InputNumber
       key={currency}
-      formatter={(value) => (currency && value ? formatPrice(value) : "")}
+      formatter={(value) =>
+        currency && value ? formatPriceWithSymbol(value, currency) : ""
+      }
       parser={(value) => (value ? value.replace(/[^0-9.]/g, "") : "")}
       {...props}
     />
