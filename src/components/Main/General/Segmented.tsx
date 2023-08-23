90876a2006d8b76f193ac34c8d2d6f35971cdb85
Ravi <rabigiri18@gmail.com>
2023-08-23 11:32:44 +0545

    fix: add primary props to segmented

--- src/components/Main/General/Segmented.tsx
+++ src/components/Main/General/Segmented.tsx
@@ -5,15 +5,17 @@ import clsx from "clsx";
 
 export default function Segmented({
   block = true,
-  className,
   size = "middle",
+  primary = true,
+  className,
   ...props
-}: SegmentedProps) {
+}: SegmentedProps & { primary?: boolean }) {
   return (
     // @ts-expect-error
     <AntdSegmented
       className={clsx(
-        "segmented-primary w-auto space-x-2 rounded-md border-2 border-solid border-neutral-4 bg-neutral-2 p-1 text-base",
+        "w-auto space-x-2 rounded-md border-2 border-solid border-neutral-4 bg-neutral-2 p-1 text-base",
+        primary ? "segmented-primary" : "",
         className
       )}
       block={block}
